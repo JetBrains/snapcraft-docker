8363435aaa8489eb5462f5ed527c7fdeb4f57bf1
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2024-06-13 19:20:58 +0200

    docker: secure curl

--- prepare.sh
+++ prepare.sh
@@ -9,7 +9,7 @@ echo "--- unpacking bases for snapcraft/$RISK ---"
 # and unpack them in the proper place.
 BASES="core core18 core20 core22"
 for base in $BASES; do
-    curl -L "$(curl -H "X-Ubuntu-Series: 16" "https://api.snapcraft.io/api/v1/snaps/details/$base" | jq ".download_url" -r)" --output "$base.snap"
+    curl -fsSL "$(curl -fsS -H "X-Ubuntu-Series: 16" "https://api.snapcraft.io/api/v1/snaps/details/$base" | jq ".download_url" -r)" --output "$base.snap"
     mkdir -p "/snap/$base"
     unsquashfs -d "/snap/$base/current" "$base.snap"
 done
@@ -19,6 +19,6 @@ echo "--- unpacking snapcraft/$RISK ---"
 
 # Grab the snapcraft snap from the $RISK channel and unpack it in the proper
 # place.
-curl -L "$(curl -H "X-Ubuntu-Series: 16" "https://api.snapcraft.io/api/v1/snaps/details/snapcraft?channel=$RISK" | jq ".download_url" -r)" --output snapcraft.snap
+curl -fsSL "$(curl -fsS -H "X-Ubuntu-Series: 16" "https://api.snapcraft.io/api/v1/snaps/details/snapcraft?channel=$RISK" | jq ".download_url" -r)" --output snapcraft.snap
 mkdir -p /snap/snapcraft
 unsquashfs -d /snap/snapcraft/current snapcraft.snap
